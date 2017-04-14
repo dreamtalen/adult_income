8849c2ab1bac7c5675acd3cc4a056b76d4f291b9
Dmtalen Ding <dreamtalen@hotmail.com>
2017-04-14 20:35:50 +0800

    增加kernel说明

--- svm.py
+++ svm.py
@@ -50,4 +50,4 @@ wrong_num = sum(1 for i, j in zip(predicted, test_y) if i != j)
 # print wrong_num
 accurate = 1 - wrong_num/float(test_set_size)
 with open('result.log', 'a') as f:
-    f.write('train set size: ' + str(train_set_size) + ' test set size: ' + str(test_set_size) + ' accurate: ' + str(accurate) + '\n')
+    f.write('kernel: ' + args.kernel + ' train set size: ' + str(train_set_size) + ' test set size: ' + str(test_set_size) + ' accurate: ' + str(accurate) + '\n')
