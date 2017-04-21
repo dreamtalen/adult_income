3972b011c8d19404e7aca46ec71a9ffa0ae5bf68
Dmtalen Ding <dreamtalen@hotmail.com>
2017-04-21 13:59:41 +0800

    加入数据预处理，对比linear和rbf

--- linear_svm.py
+++ linear_svm.py
@@ -1,11 +1,10 @@
 import numpy as np
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn import preprocessing
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('train_set_size')
 parser.add_argument('test_set_size')
-parser.add_argument('argument')
 args = parser.parse_args()
 
 train_set = np.loadtxt('adult.data.csv', delimiter=',')
@@ -21,23 +20,27 @@ y = train_set[:train_set_size, -1]
 X = preprocessing.scale(X)
 
 # normalize the data attributes
-# X = preprocessing.normalize(X)
+# normalized_X = preprocessing.normalize(X)
 
+# standardize the data attributes
+# standardized_X = preprocessing.scale(X)
+
+
+# print X[0]
+# print scaled_X[0]
+# print normalized_X[0]
+# print standardized_X[0]
+
+# X = scaled_X
 
 test_X = test_set[:test_set_size, :-1]
-
 test_y = test_set[:test_set_size, -1]
 
-#scale the data attributes
+# normalized__test_X = preprocessing.normalize(test_X)
 test_X = preprocessing.scale(test_X)
 
-# normalize the data attributes
-# test_X = preprocessing.normalize(test_X)
 
-
-clf = SVC(kernel=args.argument, cache_size=4000) #86%
-# print clf
-# clf.fit(X, y)
+clf = LinearSVC()
 clf.fit(X, y)
 
 predicted = clf.predict(test_X)
@@ -47,4 +50,4 @@ wrong_num = sum(1 for i, j in zip(predicted, test_y) if i != j)
 # print wrong_num
 accurate = 1 - wrong_num/float(test_set_size)
 with open('result.log', 'a') as f:
-    f.write(args.argument + ' '+ str(train_set_size) + '/' + str(test_set_size) + ' accurate: ' + str(accurate) + '\n')
+    f.write('LinearSVM'  + str(train_set_size) + '/' + str(test_set_size) + ' accurate: ' + str(accurate) + '\n')
